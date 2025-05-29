e3bc2aa5ea9bfc01fc71bad824d2b98d82ae5809
kinako1415 <Owata09@outlook.jp>
2025-05-29 14:52:25 +0900

    しっかり削除しました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
-"use client";
-import { Button } from "@/components/elements/Button";
-import { SocialButton } from "@/components/elements/SocialButton";
-
 export default function Home() {
-  const handleAppleLogin = () => {
-    console.log("Apple login clicked");
-  };
-
-  const handleGoogleLogin = () => {
-    console.log("Google login clicked");
-  };
-
   return <div>homeです</div>;
 }
