9e1281c1b5af1edeb4f80fccf95f97fbd49685c8
kinako1415 <Owata09@outlook.jp>
2025-05-29 12:40:50 +0900

    不要なコンポーネントとコードを削除し、ホーム画面を簡素化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,9 @@
 "use client";
-import { Button } from "@/components/elements/Button";
-import { InputField } from "@/components/elements/Input";
-import Image from "next/image";
 
 export default function Home() {
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
-  };
   return (
     <div>
       <p>ホーム画面です</p>
-      <Button>sdfadssf</Button>
-      <Button variant="outline">sdfadssf</Button>
-      <Button disabled>sdfadssf</Button>
-      <InputField
-        placeholder="input"
-        onChange={handleInputChange}
-        label="input"
-      ></InputField>
-      <InputField
-        errors="This field is required"
-        placeholder="input"
-        onChange={handleInputChange}
-        label="input"
-      ></InputField>
-      <InputField
-        isPassword
-        variant="filled"
-        placeholder="input"
-        onChange={handleInputChange}
-        label="input"
-      ></InputField>
     </div>
   );
 }
