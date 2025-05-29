6d02e367202db5e821e20dce5797b275542c5f8f
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-05-29 13:54:27 +0900

    friendのサンプルデータを用意して、cardにpropsで渡して表示

---
+++ src/app/Friend/page.tsx
@@ -0,0 +1,35 @@
+import { FriendCard } from "./components/friendCard";
+import style from "./index.module.scss";
+import icon from "@/assets/userIcon.svg";
+
+export const friendList: Friend[] = [
+  { uid: 1, name: "田中", userIcon: icon },
+  { uid: 2, name: "佐藤", userIcon: icon },
+  { uid: 3, name: "鈴木", userIcon: icon },
+  { uid: 4, name: "山本", userIcon: icon },
+];
+
+type Friend = {
+  uid: number;
+  name: string;
+  userIcon: string;
+};
+
+export default async function Page() {
+  return (
+    <>
+      <div className={style.wrapper}>
+        <h2>現在のフレンド</h2>
+        <div className={style.content}>
+          {friendList.map((friend) => (
+            <FriendCard
+              key={friend.uid}
+              name={friend.name}
+              userIcon={friend.userIcon}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -5,8 +5,11 @@ import Image from "next/image";
 import homeIcon from "../../assets/navItem.svg";
 import postIcon from "../../assets/Post.svg";
 import friendIcon from "../../assets/friend.svg";
+import { useRouter } from "next/navigation";
 
 export const Footer = () => {
+  const router = useRouter();
+
   return (
     <>
       <div className={style.border}>
@@ -17,6 +20,9 @@ export const Footer = () => {
             alt="ホームの画像"
             width={100}
             height={100}
+            onClick={() => {
+              router.push("/");
+            }}
           />
           <Image
             className={style.homeImg}
@@ -31,6 +37,9 @@ export const Footer = () => {
             alt="ホームの画像"
             width={100}
             height={100}
+            onClick={() => {
+              router.push("./friend");
+            }}
           />
         </div>
       </div>
