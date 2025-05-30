903402cbe926bf23d50db3e5aa313854c8de685e
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-05-30 19:37:05 +0900

    friendID見た目完成

--- src/app/Friend/components/select.tsx
+++ src/app/Friend/components/select.tsx
@@ -4,6 +4,9 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import style from "./select.module.scss";
 import { FriendCard } from "./friendCard";
 import icon from "@/assets/userIcon.svg";
+import Image from "next/image";
+import qr from "@/assets/QRcode Icon.svg";
+import { useRouter } from "next/navigation";
 
 const friendList: Friend[] = [
   { uid: 1, name: "田中", userIcon: icon },
@@ -18,33 +21,48 @@ type Friend = {
   userIcon: string;
 };
 
-export const Select = () => (
-  <Tabs className={style.tabs}>
-    <TabList className={style.tabList}>
-      <Tab className={style.tab} selectedClassName={style.tabSelected}>
-        申請中
-      </Tab>
-      <Tab className={style.tab} selectedClassName={style.tabSelected}>
-        保留中
-      </Tab>
-    </TabList>
-
-    <TabPanel>
-      <div className={style.wrapper}>
-        <div className={style.content}>
-          {friendList.map((friend) => (
-            <FriendCard
-              key={friend.uid}
-              name={friend.name}
-              userIcon={friend.userIcon}
-              isApply={true}
-            />
-          ))}
-        </div>
+export const Select = () => {
+  const router = useRouter();
+  return (
+    <>
+      <div
+        onClick={() => {
+          router.push("./friend/111111");
+        }}
+        className={style.qrCode}
+      >
+        <Image src={qr} alt="QRコードの画像" width={70} height={70} />
+        <p className={style.qrText}>QRコード作成 / ID検索</p>
       </div>
-    </TabPanel>
-    <TabPanel>
-      <h2>Any content 2</h2>
-    </TabPanel>
-  </Tabs>
-);
+      <Tabs className={style.tabs}>
+        <TabList className={style.tabList}>
+          <Tab className={style.tab} selectedClassName={style.tabSelected}>
+            申請中
+          </Tab>
+          <Tab className={style.tab} selectedClassName={style.tabSelected}>
+            保留中
+          </Tab>
+        </TabList>
+
+        <TabPanel>
+          <div className={style.wrapper}>
+            <div className={style.content}>
+              {friendList.map((friend) => (
+                <FriendCard
+                  key={friend.uid}
+                  name={friend.name}
+                  userIcon={friend.userIcon}
+                  isApply={true}
+                />
+              ))}
+            </div>
+          </div>
+        </TabPanel>
+
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
+    </>
+  );
+};
