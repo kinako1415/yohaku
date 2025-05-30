81e8a533e06113d5e69cde251f01d0fc4fdf062b
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-05-30 18:06:01 +0900

    フレンドページにQRコード表示を追加

--- src/app/Friend/page.tsx
+++ src/app/Friend/page.tsx
@@ -2,6 +2,8 @@ import { FriendCard } from "./components/friendCard";
 import { Select } from "./components/select";
 import style from "./index.module.scss";
 import icon from "@/assets/userIcon.svg";
+import Image from "next/image";
+import qr from "@/assets/QRcode Icon.svg";
 
 const friendList: Friend[] = [
   { uid: 1, name: "田中", userIcon: icon },
@@ -19,6 +21,10 @@ type Friend = {
 export default function Page() {
   return (
     <>
+      <div className={style.qrCode}>
+        <Image src={qr} alt="QRコードの画像" width={70} height={70} />
+        <p className={style.qrText}>QRコード作成 / ID検索</p>
+      </div>
       <Select />
       <div className={style.wrapper}>
         <h2>現在のフレンド</h2>
