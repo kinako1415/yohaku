68307eb757f2f21f1cbb56bdfcbd21b99b4bcff5
kinako1415 <Owata09@outlook.jp>
2025-05-31 00:38:41 +0900

    signInスキーマのエラーメッセージを改善

--- src/schemas/signIn.ts
+++ src/schemas/signIn.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 
 export const signInSchema = z.object({
-  email: z.string().email("これはメールアドレス??"),
+  email: z.string().email("メールアドレスの形式ではないです。"),
   password: z
     .string()
-    .min(1, "0文字だよ?しっかり入力して!!!")
-    .min(6, "登録するとき6文字以上だった気がする")
-    .regex(/^[\x20-\x7E]+$/, "英語か数字か記号以外は設定できないはず!!!!"),
+    .min(1, "文字を入力してください。")
+    .min(6, "6文字以上で入力してください。")
+    .regex(/^[\x20-\x7E]+$/, "英数字と記号のみを使用してください。"),
 });
 
 export type signInValue = z.infer<typeof signInSchema>;
