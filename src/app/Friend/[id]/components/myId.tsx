5245ebdd389465a05c721181d2faa3e19e046cba
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-05-31 12:47:05 +0900

    QRコード生成完成

--- src/app/Friend/[id]/components/myId.tsx
+++ src/app/Friend/[id]/components/myId.tsx
@@ -1,18 +1,29 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import style from "./myId.module.scss";
-import qrCode from "@/assets/qrCode.svg";
 import Image from "next/image";
+
+import ReactQRCode from "react-qr-code";
+
 import copy from "@/assets/copy.svg";
 import camera from "@/assets/camera.svg";
 import share from "@/assets/share.svg";
 import fot from "@/assets/fot.svg";
+import yohaku from "@/assets/yohaku.svg";
 
 export const MyId = () => {
   const pathname = usePathname();
   const pathArr = pathname.split("/");
   const userId = pathArr[pathArr.length - 1];
+  const router = useRouter();
+
+  const profileUrl = `https://www.figma.com/design/bLlkKV8BA7aChp4pOEUSAU/Yo-haku?node-id=293-1080&t=5kzOVmQWoJHYIAQH-0`;
+
+  const handleCameraClick = () => {
+    router.push(`/friend/${userId}/scan`);
+  };
+
   return (
     <>
       <div className={style.content}>
@@ -29,10 +40,19 @@ export const MyId = () => {
           </p>
         </div>
         <div className={style.qrCode}>
-          <Image src={qrCode} alt="QRコードの例です" width={230} height={230} />
+          <ReactQRCode
+            value={profileUrl}
+            size={230}
+            bgColor="#fff"
+            fgColor="#3fe57b"
+            level="H"
+          />
+          <div className={style.icon}>
+            <Image src={yohaku} alt="中央アイコン" width={64} height={64} />
+          </div>
         </div>
         <div className={style.button}>
-          <Image src={camera} alt="カメラのボタン" width={65} height={65} />
+          <Image  src={camera} alt="カメラのボタン" width={65} height={65} />
           <Image src={share} alt="カメラのボタン" width={65} height={65} />
           <Image src={fot} alt="カメラのボタン" width={65} height={65} />
         </div>
