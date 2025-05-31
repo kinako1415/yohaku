d87b5b3ce8e3ed5a693b1d977bc8ec71e1ef9070
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-05-31 15:59:11 +0900

    1秒後にコピーが完了しましたが消えるようになった

--- src/app/Friend/[id]/components/myId.tsx
+++ src/app/Friend/[id]/components/myId.tsx
@@ -10,12 +10,14 @@ import camera from "@/assets/camera.svg";
 import share from "@/assets/share.svg";
 import fot from "@/assets/fot.svg";
 import yohaku from "@/assets/yohaku.svg";
+import { useEffect, useState } from "react";
 
 export const MyId = () => {
   const pathname = usePathname();
   const pathArr = pathname.split("/");
   const userId = pathArr[pathArr.length - 1];
   const router = useRouter();
+  const [copied, setCopied] = useState(false);
 
   //追加ボタンがあるリンクに飛ばす
   const profileUrl = `https://www.figma.com/design/bLlkKV8BA7aChp4pOEUSAU/Yo-haku?node-id=293-1080&t=5kzOVmQWoJHYIAQH-0`;
@@ -35,6 +37,25 @@ export const MyId = () => {
     }
   };
 
+  const handleCopyClick = async () => {
+    try {
+      await navigator.clipboard.writeText(userId);
+      setCopied(true);
+    } catch {
+      console.error("コピー失敗");
+    }
+  };
+
+  //数秒後に消えるりょ離
+  useEffect(() => {
+    if (copied) {
+      // copiedがtrueになったら3秒後にfalseに戻すタイマーを設定
+      setTimeout(() => {
+        setCopied(false);
+      }, 1000);
+    }
+  }, [copied]);
+
   return (
     <>
       <div className={style.content}>
@@ -42,14 +63,17 @@ export const MyId = () => {
           <p className={style.idText}>
             My ID:{userId}
             <Image
+              onClick={handleCopyClick}
               className={style.coptImg}
               src={copy}
               alt="copyの画像"
               width={23}
               height={23}
+              style={{ cursor: "pointer" }}
             />
           </p>
         </div>
+        <div className={style.copy}>{copied ? "コピーが完了しました" : ""}</div>
         <div className={style.qrCode}>
           <QRCodeCanvas
             value={profileUrl}
