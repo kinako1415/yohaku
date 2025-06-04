40f3bcc0fa291b8ba5a7a603263368d72d9a1464
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-06-04 11:45:30 +0900

    QRコード読み取り完成

--- src/app/Friend/scan/components/qrScan.tsx
+++ src/app/Friend/scan/components/qrScan.tsx
@@ -1,65 +1,34 @@
 "use client";
 
-import { useRef, useState, useEffect } from "react";
-import { Html5Qrcode } from "html5-qrcode";
-import style from "./scan.module.scss";
+import React, { useState } from "react";
+import { useZxing } from "react-zxing";
+import styles from "./scan.module.scss";
 
 export const QrScan = () => {
-  const [decodedText, setDecodedText] = useState<string | null>(null);
-  const scannerRef = useRef<Html5Qrcode | null>(null);
-  const [isStarted, setIsStarter] = useState<boolean>(false);
-
-  useEffect(() => {
-    // 既にスキャナーが初期化されている場合は何もしない
-    if (scannerRef.current) {
-      return;
-    }
-
-    const scanner = new Html5Qrcode("reader");
-    scannerRef.current = scanner;
-
-    const startCamera = async () => {
-      if (isStarted) return;
-
-      try {
-        const devices = await Html5Qrcode.getCameras();
-        if (devices.length === 0) {
-          console.warn("カメラが見つかりませんでした。");
-          return;
-        }
-
-        setIsStarter(true); // 開始フラグをセット
-
-        await scanner.start(
-          { facingMode: "environment" },
-          { fps: 10, qrbox: 250 },
-          (text) => {
-            setDecodedText(text);
-            // 読み取り成功後、スキャナーを停止しクリア
-            scanner.stop().then(() => scanner.clear());
-            setIsStarter(false);
-          },
-          (err) => console.warn("読み取りエラー:", err)
-        );
-      } catch (err) {
-        console.error("カメラ初期化失敗:", err);
-      }
-    };
-
-    startCamera(); // コンポーネントマウント時にカメラを起動
-  }, []);
+  const [result, setResult] = useState("");
+  const { ref } = useZxing({
+    onDecodeResult(result) {
+      setResult(result.getText());
+    },
+  });
 
   return (
-    <div className={style.content}>
-      <div id="reader" className={style.reader} />
-      {decodedText && (
-        <div>
-          <p>読み取った内容:</p>
-          <a href={decodedText} target="_blank" rel="noopener noreferrer">
-            {decodedText}
-          </a>
+    <div>
+      <div className={styles.wrapper}>
+        <video ref={ref} className={styles.video} />
+        <div className={styles.mask}>
+          <div className={styles.top}></div>
+          <div className={styles.bottom}></div>
+          <div className={styles.left}></div>
+          <div className={styles.right}></div>
         </div>
-      )}
+        <div className={styles.scanLine}></div>
+
+        <div className={styles.scanBox}></div>
+      </div>
+      <p style={{ textAlign: "center", color: "#0f0" }}>
+        Last result: {result}
+      </p>
     </div>
   );
 };
