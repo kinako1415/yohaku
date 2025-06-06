17ff313f0bec2b23ab22091f1890d739d2ada3ee
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-06-06 10:59:35 +0900

    写真を変更しいて反映できるようにした

--- src/app/myPage/setting/page.tsx
+++ src/app/myPage/setting/page.tsx
@@ -1,25 +1,13 @@
 import style from "./index.module.scss";
 
-import Image from "next/image";
-import icon from "@/assets/userIcon.svg";
-import change from "@/assets/change.svg";
+import { ChangeImage } from "./components/changeImage";
 import { SettingInput } from "./components/settingInput";
 
 export default function Page() {
   return (
     <>
       <div className={style.content}>
-        <div className={style.userIcon}>
-          <Image src={icon} alt="userIcon" width={130} height={130} />
-          <Image
-            className={style.change}
-            style={{ cursor: "pointer" }}
-            src={change}
-            alt="userIcon"
-            width={100}
-            height={50}
-          />
-        </div>
+        <ChangeImage />
         <div className={style.inputName}>
           <h4>ニックネーム</h4>
           <SettingInput placeholder="yuki_cafe" />
