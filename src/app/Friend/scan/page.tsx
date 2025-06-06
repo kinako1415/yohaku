3af6a4aecbcfbbd297ba552d775490b9d80d4a7b
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-06-06 16:37:19 +0900

    fontを変更した

--- src/app/Friend/scan/page.tsx
+++ src/app/Friend/scan/page.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { QrScan } from "./components/qrScan";
 import style from "./index.module.scss"; // Adjust the path as necessary
+import { Noto_Sans_Javanese } from "next/font/google";
+
+const NotoSansJavanese = Noto_Sans_Javanese({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function Page() {
   return (
     <div className={style.container}>
       <div className={style.text}>
-        <h3 className={style.title}>
+        <h2 className={style.title}>
           フレンドQR を<br />
           よみとってください
-        </h3>
+        </h2>
+        <div>
+          <div className={NotoSansJavanese.className}>
+            <p className={style.detail}>
+              このQRコードを読み取ることで、簡単に友達を追加できます。対面での友達追加やイベント時の連絡先交換にご活用ください。
+            </p>
+          </div>
+        </div>
       </div>
       <QrScan />
     </div>
