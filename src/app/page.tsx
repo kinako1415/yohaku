186da7e0d5b28263a73417595d099ad7d4212f0e
nrak126 <daichidaichi126@gmail.com>
2025-06-07 18:08:45 +0900

    lintエラーの解消

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-import { getUserById } from "@/actions/user/getUserById";
 import { HomeCalendar } from "./home/component/HomeCalendar";
-import { User } from "@/types";
 
 export default async function Home() {
-  const user: User = await getUserById("SctU5ffUnAhX7VMtbWuA");
-  console.log("Fetched users:", user);
   return (
     <div>
       <HomeCalendar />
