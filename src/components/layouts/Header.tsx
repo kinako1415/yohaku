3c04c6cf36f6117646f8c9c88a8fcce988bd54c0
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-06-08 01:22:59 +0900

    Headerのcss修正

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -16,51 +16,42 @@ export const Header: React.FC<HeaderProps> = (props) => {
 
   const isMyPage = pathname === "/myPage";
   return (
-    <div className={style.headerContainer}>
-      <div className={style.container}>
-        <div className={style.row}>
-          {isCheck && (
-            <div
-              className={style.backButton}
-              onClick={() => window.history.back()}
-              style={{ cursor: "pointer" }}
-            >
-              <Image src={backIcon} alt="Back" width={24} height={24} />
-            </div>
-          )}
+    <div className={style.container}>
+      <div className={style.titleLeft}>
+        {isCheck && (
           <div
-            className={style.header}
-            style={isCheck ? { position: "absolute", left: "7%" } : {}}
+            className={style.backButton}
+            onClick={() => window.history.back()}
+            style={{ cursor: "pointer" }}
           >
-            {title}
+            <Image src={backIcon} alt="Back" width={24} height={24} />
           </div>
-
-          <div className={style.space} />
-
-          <div />
-          {isMyPage ? (
-            <Link href="/myPage/setting">
-              <Image
-                className={style.icon}
-                src={settingIcon}
-                alt="icon"
-                width={40}
-                height={40}
-              />
-            </Link>
-          ) : (
-            <Link href="/myPage">
-              <Image
-                className={style.icon}
-                src={icon}
-                alt="icon"
-                width={40}
-                height={40}
-              />
-            </Link>
-          )}
-        </div>
+        )}
+        <div className={style.header}>{title}</div>
       </div>
+
+      <div />
+      {isMyPage ? (
+        <Link href="/myPage/setting">
+          <Image
+            className={style.icon}
+            src={settingIcon}
+            alt="icon"
+            width={40}
+            height={40}
+          />
+        </Link>
+      ) : (
+        <Link href="/myPage">
+          <Image
+            className={style.icon}
+            src={icon}
+            alt="icon"
+            width={40}
+            height={40}
+          />
+        </Link>
+      )}
     </div>
   );
 };
