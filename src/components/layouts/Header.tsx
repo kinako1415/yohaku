9d03f70a078fc691d49ba55043b689777803de6d
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-06-08 01:39:49 +0900

    Headerコンポーネントの構造を整理し、不要なdivを削除して可読性を向上

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -17,42 +17,45 @@ export const Header: React.FC<HeaderProps> = (props) => {
 
   const isMyPage = pathname === "/myPage";
   return (
-    <div className={style.container}>
-      <div className={style.titleLeft}>
-        {isCheck && (
-          <div
-            className={style.backButton}
-            onClick={() => window.history.back()}
-            style={{ cursor: "pointer" }}
-          >
-            <Image src={backIcon} alt="Back" width={24} height={24} />
-          </div>
-        )}
-        <div className={style.header}>{title}</div>
-      </div>
+    <>
+      <div className={style.container}>
+        <div className={style.titleLeft}>
+          {isCheck && (
+            <div
+              className={style.backButton}
+              onClick={() => window.history.back()}
+              style={{ cursor: "pointer" }}
+            >
+              <Image src={backIcon} alt="Back" width={24} height={24} />
+            </div>
+          )}
+          <div className={style.header}>{title}</div>
+        </div>
 
-      <div />
-      {isMyPage ? (
-        <Link href="/myPage/setting">
-          <Image
-            className={style.icon}
-            src={settingIcon}
-            alt="icon"
-            width={40}
-            height={40}
-          />
-        </Link>
-      ) : (
-        <Link href="/myPage">
-          <Image
-            className={style.icon}
-            src={userIcon ?? icon}
-            alt="icon"
-            width={40}
-            height={40}
-          />
-        </Link>
-      )}
-    </div>
+        <div />
+        {isMyPage ? (
+          <Link href="/myPage/setting">
+            <Image
+              className={style.icon}
+              src={settingIcon}
+              alt="icon"
+              width={40}
+              height={40}
+            />
+          </Link>
+        ) : (
+          <Link href="/myPage">
+            <Image
+              className={style.icon}
+              src={userIcon ?? icon}
+              alt="icon"
+              width={40}
+              height={40}
+            />
+          </Link>
+        )}
+      </div>
+      <div className={style.space}/>
+    </>
   );
 };
