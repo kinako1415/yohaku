1d3568670813f4bd31f1e04d4b21b92307eabfbe
nrak126 <daichidaichi126@gmail.com>
2025-06-12 18:07:29 +0900

    Revert "Merge pull request #139 from kinako1415/feature/yohaku参加機能"
    This reverts commit a348863c9147cc77620c61afe5c3d6ce23963f05, reversing
    changes made to c94c256a039b0a758049d1e8db420ab944b5839d.

--- src/app/home/component/JoinButton.tsx
+++ src/app/home/component/JoinButton.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@/components/elements/Button";
 import { useRouter } from "next/navigation";
 
-type Props = {
-  handleJoin: () => void;
-}
+export const JoinButton = () => {
+  const router = useRouter();
 
-export const JoinButton: React.FC<Props> = ({ handleJoin }) => {
+  const handleJoin = () => {
+    router.push("/message");
+  };
+
   return (
     <Button type="submit"  onClick={handleJoin}>
       参加する
